55a8dfee03d4752e71d22c13c03cad60c3994024
benrkia <fatimazahrae.benrkia@gmail.com>
2024-02-29 23:28:34 +0000

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-"""Making change"""
+'''Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet
+    a given amount total.
+'''
+import sys
 
 
 def makeChange(coins, total):
-    """making change"""
+    '''
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    '''
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
     for i in range(1, total + 1):
-        for coin in coins:
-            if i >= coin:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
 
-    return dp[total] if dp[total] != float('inf') else -1
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
